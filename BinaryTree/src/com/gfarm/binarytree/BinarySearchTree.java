189f6139dc00d2252781ac1767879fce8acfe713
chetanbg1 <chetanbg00@gmail.com>
2024-08-01 18:52:40 +0530

    updated binary tree

--- BinaryTree/src/com/gfarm/binarytree/BinarySearchTree.java
+++ BinaryTree/src/com/gfarm/binarytree/BinarySearchTree.java
@@ -1,115 +1,173 @@
 package com.gfarm.binarytree;
 
+import java.util.ArrayList;
+
 public class BinarySearchTree {
-	
+
 	private TreeNode root;
-	
-	private class TreeNode{
+
+	private class TreeNode {
 		private int data;
 		private TreeNode left;
 		private TreeNode right;
-		
+
 		public TreeNode(int data) {
 			this.data = data;
 		}
 	}
-	
+
 	// insert recursive
-	
+
 	public void insert(int data) {
 		root = insert(root, data);
 	}
+
 	public TreeNode insert(TreeNode root, int value) {
-		if(root ==null) {
+		if (root == null) {
 			root = new TreeNode(value);
 			return root;
 		}
-		if(value < root.data) {
-			root.left = insert(root.left , value);
+		if (value < root.data) {
+			root.left = insert(root.left, value);
 		}
-		if(value > root.data) {
-			root.right = insert(root.right , value);
+		if (value > root.data) {
+			root.right = insert(root.right, value);
 		}
 		return root;
 	}
-	//delete node
-	public TreeNode delete(TreeNode root , int val) {
-		if(root.data > val) {
-			root.left = delete(root.left , val);
-		}else if(root.data < val){
+
+	// delete node
+	public TreeNode delete(TreeNode root, int val) {
+		if (root.data > val) {
+			root.left = delete(root.left, val);
+		} else if (root.data < val) {
 			root.right = delete(root.right, val);
-		}else {
-			//case 1 no child
-			if(root.left == null && root.right == null) {
+		} else {
+			// case 1 no child
+			if (root.left == null && root.right == null) {
 				return null;
 			}
-			//case 2 with 1 child
-			if(root.left == null) {
+			// case 2 with 1 child
+			if (root.left == null) {
 				return root.right;
-			}else if(root.right == null) {
+			} else if (root.right == null) {
 				return root.left;
 			}
-			//case 3 having 2 child nodes
+			// case 3 having 2 child nodes
 			TreeNode is = inorderSuccessor(root.right);
-			root.data = is.data;
+			// inorder successor id left most element from the root
+			root.data = is.data; // replacing root data with Inorder Successor
 			root.right = delete(root.right, is.data);
 		}
 		return root;
 	}
+
 	private TreeNode inorderSuccessor(TreeNode root) {
-		while(root.left != null) {
+		while (root.left != null) {
 			root = root.left;
 		}
 		return root;
 	}
-	// print 
+
+	// print
 	public void preorder(TreeNode root) {
-		if(root ==null) {
+		if (root == null) {
 			return;
 		}
 		System.out.print(root.data + " ");
 		preorder(root.left);
 		preorder(root.right);
 	}
-	
-	//inorder
+
+	// inorder
 	public void inorder(TreeNode root) {
-		if(root ==null) {
+		if (root == null) {
 			return;
 		}
-		
+
 		inorder(root.left);
 		System.out.print(root.data + " ");
 		inorder(root.right);
 	}
-	
-	//serarch
+
+	// serarch
 	public TreeNode search(TreeNode root, int key) {
-		if(root ==null || root.data == key) {
+		if (root == null || root.data == key) {
 			return root;
 		}
-		if(key < root.data) {
-			return search(root.left , key);
-		}else{
-			return search(root.right , key);
+		if (key < root.data) {
+			return search(root.left, key);
+		} else {
+			return search(root.right, key);
 		}
 	}
-	
+
+	// serarch
+	public boolean search1(TreeNode root, int key) {
+		if (root == null) {
+			return false;
+		}
+		if (key < root.data) {
+			return search1(root.left, key);
+		} else if (root.data == key) {
+			return true;
+		} else {
+			return search1(root.right, key);
+		}
+	}
+
 	// valid
 	public boolean isValid(TreeNode root, long min, long max) {
-		if(root == null) {
+		if (root == null) {
 			return true;
 		}
-		if(root.data <= min || root.data >=min) {
+		if (root.data <= min || root.data >= min) {
 			return false;
 		}
 		boolean left = isValid(root.left, min, root.data);
-		if(left) {
+		if (left) {
 			boolean right = isValid(root.right, root.data, max);
 			return right;
 		}
 		return false;
 	}
+
+	// print range
+	public void printInRange(TreeNode root, int x, int y) {
+		if (root == null) {
+			return;
+		}
+		if (x <= root.data && root.data <= y) {
+			printInRange(root.left, x, y);
+			System.out.print(root.data + " ");
+			printInRange(root.right, x, y);
+		} else if (root.data >= y) {
+			printInRange(root.left, x, y);
+		} else
+			printInRange(root.right, x, y);
+	}
+	//root to leave paths
+	public void printRootToLeaf(TreeNode root, ArrayList<Integer> path) {
+		if(root ==null) {
+			return;
+		}
+		path.add(root.data);
+		if(root.left ==null && root.right==null) {
+			printPath(path);
+		}else {
+		printRootToLeaf(root.left, path);
+		printRootToLeaf(root.right, path);
+		}
+		path.remove(path.size()-1);
+	}
+	private void printPath(ArrayList<Integer> path) {
+		for(int i :path) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		
+	}
+
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
 		tree.insert(8);
@@ -121,18 +179,22 @@ public class BinarySearchTree {
 		tree.insert(10);
 		tree.insert(11);
 		tree.insert(14);
-		//System.out.println(tree.insert(tree.root, 5).data);
+		// System.out.println(tree.insert(tree.root, 5).data);
 		
-		
-		tree.preorder(tree.root);
-		System.out.println();
-		tree.inorder(tree.root);
-		tree.delete(tree.root, 5);
-		System.out.println();
-		tree.inorder(tree.root);
-		//System.out.println();
-		//System.out.println(tree.search(tree.root, 3).data);
-		//System.out.println(tree.isValid(tree.root, -100, 100));
+//		System.out.println(tree.search1(tree.root, 89));
+//		tree.preorder(tree.root);
+//		System.out.println();
+//		tree.inorder(tree.root);
+//		tree.delete(tree.root, 5);
+//		System.out.println();
+//		tree.inorder(tree.root);
+//		System.out.println();
+//		tree.printInRange(tree.root, 4, 11);
+		ArrayList<Integer> path = new ArrayList<>();
+		tree.printRootToLeaf(tree.root, path);
+		// System.out.println();
+		// System.out.println(tree.search(tree.root, 3).data);
+		// System.out.println(tree.isValid(tree.root, -100, 100));
 
 	}
 
--- BinaryTree/src/com/gfarm/binarytree/BinaryTree.java
+++ BinaryTree/src/com/gfarm/binarytree/BinaryTree.java
@@ -7,15 +7,27 @@ import java.util.Stack;
 public class BinaryTree {
 
 	private TreeNode root;
+	static int idx = -1;
 
 	private class TreeNode {
 		private int data;
 		private TreeNode right;
 		private TreeNode left;
 
-		public TreeNode(int data) {
+		public TreeNode(int data) { // constructor
 			this.data = data;
 		}
+	}
+
+	public TreeNode buildTree(int node[]) {
+		idx++;
+		if (node[idx] == -1) {
+			return null;
+		}
+		TreeNode newNode = new TreeNode(node[idx]);
+		newNode.left = buildTree(node);
+		newNode.right = buildTree(node);
+		return newNode;
 	}
 
 	public void createBinaryTree() {
@@ -40,7 +52,6 @@ public class BinaryTree {
 			return;
 		}
 		System.out.print(root.data + " ");
-
 		preOrder(root.left);
 		preOrder(root.right);
 	}
@@ -100,7 +111,7 @@ public class BinaryTree {
 				if (temp == null) {
 					temp = stack.pop();
 					System.out.print(temp.data + " ");
-					while(!stack.isEmpty() && temp ==stack.peek().right){
+					while (!stack.isEmpty() && temp == stack.peek().right) {
 						temp = stack.pop();
 						System.out.print(temp.data + " ");
 					}
@@ -131,59 +142,279 @@ public class BinaryTree {
 		System.out.print(root.data + " ");
 
 	}
-	
-	//level order traversal 
+
+	// level order traversal
 	public void levelTrav() {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
 		Queue<TreeNode> que = new LinkedList<>();
 		que.offer(root);
-		while(!que.isEmpty()){
+		while (!que.isEmpty()) {
 			TreeNode temp = que.poll();
 			System.out.print(temp.data + " ");
-			if(temp.left != null) {
+			if (temp.left != null) {
 				que.offer(temp.left);
 			}
-			if(temp.right != null) {
+			if (temp.right != null) {
 				que.offer(temp.right);
 			}
 		}
 	}
-	//find max value in binary tree recursive
+
+	//
+	public void levelTrav1(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		Queue<TreeNode> que = new LinkedList<>();
+		que.offer(root);
+		que.add(null);
+		while (!que.isEmpty()) {
+			TreeNode temp = que.poll();
+			if (temp == null) {
+				System.out.println();
+				if (que.isEmpty()) {
+					break;
+				} else {
+					que.add(null);
+				}
+			} else {
+				System.out.print(temp.data + " ");
+				if (temp.left != null) {
+					que.offer(temp.left);
+				}
+				if (temp.right != null) {
+					que.offer(temp.right);
+				}
+			}
+		}
+	}
+
+	// find max value in binary tree recursive
 	public int maxValue(TreeNode root) {
-		if(root ==null) {
+		if (root == null) {
 			return 0;
 		}
 		int result = root.data;
 		int left = maxValue(root.left);
 		int right = maxValue(root.right);
-		if(left > result) {
-			result =  left;
+		if (left > result) {
+			result = left;
 		}
-		if(right > result) {
+		if (right > result) {
 			result = right;
 		}
 		return result;
-		
+
 	}
+
+	// count the nodes
+	public int countNode(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int leftNodes = countNode(root.left);
+		int rightNodes = countNode(root.right);
+
+		return leftNodes + rightNodes + 1;
+
+	}
+
+	// sum of nodes
+	public int sumofNodes(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int result = root.data;
+		int leftSum = sumofNodes(root.left);
+		int rightSum = sumofNodes(root.right);
+
+		return result + leftSum + rightSum;
+	}
+
+	// height of Tree
+	public int heightOfTree(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int leftHeight = heightOfTree(root.left);
+		int rightHeight = heightOfTree(root.right);
+		return Math.max(leftHeight, rightHeight) + 1;
+//		if(leftHeight > rightHeight) {
+//			return leftHeight +1;
+//		}else 
+//			 return rightHeight +1;
+	}
+
+	// diameter of the tree
+	// number of nodes in the longest path between any2 nodes
+	// case1 -- through the root
+	// case 2 -- not through the root
+	public int diameter(TreeNode root) { // O(n^2)
+		if (root == null) {
+			return 0;
+		}
+		int diam1 = diameter(root.left);
+		int diam2 = diameter(root.right);
+		int diam3 = heightOfTree(root.left) + heightOfTree(root.right) + 1;
+
+		return Math.max(diam3, Math.max(diam1, diam2));
+
+	}
+
+	// diameter of the tree in O(n)
+	static class TreeInfo {
+		int ht; // height
+		int diam; // diameter
+
+		public TreeInfo(int ht, int diam) {
+			super();
+			this.ht = ht;
+			this.diam = diam;
+		}
+
+	}
+
+	public TreeInfo diameter1(TreeNode root) {
+		if (root == null) {
+			return new TreeInfo(0, 0);
+		}
+		TreeInfo left = diameter1(root.left);
+		TreeInfo right = diameter1(root.right);
+
+		int myheight = Math.max(left.ht, right.ht) + 1;
+		int diam1 = left.diam;
+		int diam2 = right.diam;
+		int diam3 = left.ht + right.ht + 1;
+
+		int myDiam = Math.max(diam3, Math.max(diam1, diam2));
+
+		TreeInfo myInfo = new TreeInfo(myheight, myDiam);
+		return myInfo;
+
+	}
+
+	// subTree of other tree
+	public boolean isSubTree(TreeNode root, TreeNode subRoot) {
+		if (subRoot == null) {
+			return true;
+		}
+		if (root == null) {
+			return false;
+		}
+		if (root.data == subRoot.data) {
+			if (isIdentical(root, subRoot)) {
+				return true;
+			}
+		}
+		return isSubTree(root.left, subRoot) || isSubTree(root.right, subRoot);
+	}
+
+	private boolean isIdentical(TreeNode root, TreeNode subRoot) {
+		if (root == null && subRoot == null) {
+			return true;
+		}
+		if (root == null || subRoot == null) {
+			return false;
+		}
+		if (root.data == subRoot.data) {
+			return isIdentical(root.left, subRoot) && isIdentical(root.right, subRoot);
+		}
+		return false;
+	}
+	//sum at the Kth level 
+	//Traverse the Binary Tree using Level Order Traversal and queue
+	//During traversal, pop each element out of the queue and push it’s child (if available) in the queue.
+	//Keep the track of the current level of the Binary tree.
+	//To track the current level,
+	//declare a variable level and increase it whenever a child is traversed from the parent.
+	//When the current level of the tree i.e. the variable level meets the required Kth level,
+	//pop the elements from the queue and calculate their sum.
+	
+	public int sumAtKthLevel(TreeNode root,int k) {
+		if(root == null) {
+			return 0;
+		}
+		
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(root);
+		int level = 0;
+		int sum = 0;
+		int flag =0;
+		while(!q.isEmpty()) {
+			int size = q.size();
+			while(size-- > 0) {
+				TreeNode current = q.peek();
+				q.remove();
+				if(level == k) {
+					flag =1;
+					sum += current.data;
+				}else {
+					if(current.left !=null) 
+						q.add(current.left);
+					
+					if(current.right !=null) 
+						q.add(current.right);
+				}
+				level++;
+				if(flag ==1) {
+					break;
+				}
+				
+			}
+			
+		}
+		return sum;
+	}
+	//sum at kth recursion
+	  public int sumAtKthLevel(TreeNode root, int k, int level) {
+	        if (root == null) {
+	            return 0; // If the tree node is null, return 0 (no sum).
+	        }
+	        if (level == k) {
+	            return root.data; // If we've reached the desired level, return the node's value.
+	        }
+	        // Recursively calculate the sum at the kth level in the left and right subtrees.
+	        return sumAtKthLevel(root.left, k, level + 1) + sumAtKthLevel(root.right, k, level + 1);
+	    }
 	public static void main(String[] args) {
 		BinaryTree tree = new BinaryTree();
-		tree.createBinaryTree();
-		// tree.preOrder(tree.root);
+		int node[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+		//int subNode [] = {2, 4, -1, -1, 5, -1, -1};
+		TreeNode root = tree.buildTree(node);
+		//TreeNode subRoot = tree.buildTree(subNode);
+//		System.out.println(root.data);
+//		// tree.createBinaryTree();
+//		tree.preOrder(root);
+//		System.out.println();
+//		tree.inOrder(root);
+//		System.out.println();
+//		tree.postOrder(root);
+//		System.out.println();
+//		tree.levelTrav1(root);
+//		System.out.println(tree.countNode(root));
+//		System.out.println(tree.sumofNodes(root));
+//		System.out.println(tree.heightOfTree(root));
+//		System.out.println(tree.diameter(root));
+//		System.out.println(tree.diameter1(root).diam);
+//		System.out.println(tree.diameter1(root).ht);
+		//System.out.println(tree.sumAtKthLevel(root, 3));
+		System.out.println(tree.sumAtKthLevel(root, 1, 0));
+		//System.out.println(tree.isSubTree(root,subRoot));
 		// System.out.println();
 		// tree.preOrderT();
 		// System.out.println();
 		// tree.inOrder(tree.root);
 		// System.out.println();
-		//tree.postOrder(tree.root);
-		//System.out.println();
+		// tree.postOrder(tree.root);
+		// System.out.println();
 		// tree.inOrderT();
-		//tree.postOrderT();
-		System.out.println();
-		//tree.levelTrav();
-		System.out.println();
-		System.out.println(tree.maxValue(tree.root));
+		// tree.postOrderT();
+		// System.out.println();
+		// tree.levelTrav();
+		// System.out.println();
+		// System.out.println(tree.maxValue(tree.root));
 	}
 
 }
